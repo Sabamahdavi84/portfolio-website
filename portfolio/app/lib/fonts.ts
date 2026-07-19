cf74d185dd4aa7a7cf55f8a11ca53e3c01e730e4
Sabamahdavi84 <sabamahdavi8400@gmail.com>
2026-07-19 08:25:11 +0330

    <main>[fonts]:edited fonts

--- portfolio/app/lib/fonts.ts
+++ portfolio/app/lib/fonts.ts
@@ -14,12 +14,12 @@ export const vazirmatn = localFont({
 
 export const inter = localFont({
   src: [
-    { path: "../fonts/inter/inter-Black.woff2", weight: "900" },
-    { path: "../fonts/inter/inter-Bold.woff2", weight: "700" },
-    { path: "../fonts/inter/inter-ExtraBold.woff2", weight: "800" },
-    { path: "../fonts/inter/inter-ExtraLight.woff2", weight: "200" },
-    { path: "../fonts/inter/inter-Light.woff2", weight: "300" },
-    { path: "../fonts/inter/inter-Medium.woff2", weight: "500" },
-    { path: "../fonts/inter/inter-SemiBold.woff2", weight: "600" },
+    { path: "../fonts/inter/Inter-Black.woff2", weight: "900" },
+    { path: "../fonts/inter/Inter-Bold.woff2", weight: "700" },
+    { path: "../fonts/inter/Inter-ExtraBold.woff2", weight: "800" },
+    { path: "../fonts/inter/Inter-ExtraLight.woff2", weight: "200" },
+    { path: "../fonts/inter/Inter-Light.woff2", weight: "300" },
+    { path: "../fonts/inter/Inter-Medium.woff2", weight: "500" },
+    { path: "../fonts/inter/Inter-SemiBold.woff2", weight: "600" },
   ],
 });
